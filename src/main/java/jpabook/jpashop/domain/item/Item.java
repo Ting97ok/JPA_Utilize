d257157465dfb65eb4c738225d4ec887578baa74
최대현 <chleogus124@gmail.com>
2021-12-22 02:55:22 +0900

    주문 도메인 개발
    casecade
    해당 엔티티에 퍼시스턴스가 일어나면 casecade로 설정된 엔티티들도 강제로 퍼시스턴스된다.
    
    !!참고사항!!
    - 해당 엔티티에서 고유하게 사용하는 경우 사용하는것이 안전하다
    - 완벽히 이해하고 사용할 것
    
    퍼시스턴스 : DB쿼리 실행?

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -37,5 +37,6 @@ public abstract class Item {
         if (restStock < 0) {
             throw new NotEnoughStockException("need more stock");
         }
+        this.stockQuantity = restStock;
     }
 }
